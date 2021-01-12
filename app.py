9d74707ad77da8839c2e6b0c7f1efa85a33abb7f
lucas <jusihyeong078@gmail.com>
2021-01-12 22:52:18 +0900

    회원가입 / 로그인 페이지 작업

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_restful import Api
 from flask_wtf import CSRFProtect
 
 from functions import number_format, date_format
+from users.views import users_app
 
 app = Flask(__name__)
 api = Api(app)
@@ -14,7 +16,8 @@ app.jinja_env.filters['date_format'] = date_format
 def create_app(config_filename, env=None):
     csrf = CSRFProtect(app)
     csrf.init_app(app)
-
+    CORS(app)
     app.config.from_object(config_filename)
+    app.register_blueprint(users_app)
 
     return app
--- users/views.py
+++ users/views.py
@@ -5,11 +5,17 @@ from users.forms import LoginForm
 users_app = Blueprint('users_app', __name__)
 
 
+# 로그인
 @users_app.route('/login', methods=['GET', 'POST'])
 def login():
-
     form = LoginForm()
 
     context = {}
 
-    return render_template('login/login.html', context=context, form=form)
+    return render_template('admin/users/login.html', context=context, form=form)
+
+
+# 회원가입
+@users_app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('admin/users/register.html')
